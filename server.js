73739b1328e9d9ac5fd8ae0732c2a8ec57b30d01
Amar Preet Singh <amarpsp10@gmail.com>
2021-11-30 15:07:07 +0530

    heroku config added

--- server.js
+++ server.js
@@ -4,12 +4,8 @@ const path = require('path')
 const app = express();
 app.use(express.static(path.join(__dirname,"dist")));
 
-app.get('*',(req, res)=>{
-    res.sendFile(path.join(__dirname,'dist/index.html'))
-})
+const PORT = process.env.PORT || 3000;
 
-const port = process.env.PORT || 3000;
-
-app.listen(3000, function listenHandler() {
-  console.info(`Running on ${port}`);
+app.listen(PORT, function listenHandler() {
+  console.info(`Running on ${PORT}`);
 });
